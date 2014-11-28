52e78578bfed852aac7d375c3baf3c0431b4e9f4
fefofunes <federicofunes92@gmail.com>
2014-11-28 02:52:30 -0300

    Nuevo archivo IngredienteModel

---
+++ src/main/java/tallerweb/sangucheto/controllers/IngredienteModel.java
@@ -0,0 +1,26 @@
+package tallerweb.sangucheto.controllers;
+
+public class IngredienteModel {
+	private String nombre;
+	private Double precio;
+	private String tipo;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
